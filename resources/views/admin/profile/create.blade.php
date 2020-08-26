4147d314b8e9bb4f7784efa23244373e666354f8
”名前” <>
2020-08-26 01:56:11 +0000

    add new file

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+<!---->課題4
+@extends('profile')
+
+@section('title','プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <body>
+                <h1>Myプロフィール作成画面</h1>
+            </body>
+        </div>
+    </div>
+@endsection
