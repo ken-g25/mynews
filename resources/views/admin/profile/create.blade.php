5c6cefd11389e004b9117d9e19a1d13c89b10c5c
”名前” <>
2020-09-03 08:02:15 +0000

    add new file

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,27 +7,30 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <body>
                 <h1>Myプロフィール作成画面</h1>
                 
                 @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
+                    <ul>
+                        @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             
                 <form action = "{{action('Admin\ProfileController@create')}}" method="post" enctype="multipart/form-data">
-                <p>名前：<input type="text" name="name"/></p>
-                <p>性別：<input type="text" name="gender"/></P>
-                        
-                <p>趣味：<input type="text" name="hobby"/></p>
-                <p>自己紹介欄</p>
-                <textarea name="introduction"></textarea>
-                
+                    <p>名前：<input type="text" name="name"/></p>
+                    <p>性別：<input type="text" name="gender"/></P>
+                            
+                    <p>趣味：<input type="text" name="hobby"/></p>
+                    <p>自己紹介欄</p>
+                    <textarea name="introduction"></textarea>
+                    <input type="file" class="form-control-file" name="image">
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
                 </form>
-            </body>
+                
         </div>
     </div>
 @endsection
